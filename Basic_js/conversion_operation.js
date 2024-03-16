091e0d4b1384f2e5ed4abd47e708e73b2e12dc9a
RsnGupta1418 <163626676+RsnGupta1418@users.noreply.github.com>
2024-03-16 16:32:09 +0000

    conversion_operation

---
+++ Basic_js/conversion_operation.js
@@ -0,0 +1,19 @@
+let score = "48aefd"
+ 
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let convertNumber = Number(score)//convert in number
+// console.log(typeof(convertNumber)) //number
+// console.log(convertNumber) //NaN
+
+// if score = null then 
+// console.log(score) => 0
+
+// let isLogin = null
+// let convertInBoolean = Boolean(isLogin)
+// console.log((convertInBoolean))
+
+let someNumber = 49494
+let cnvtInString = String(someNumber)
+console.log(cnvtInString)  // 49494 as a string
--- Basic_js/text.js
+++ Basic_js/text.js
@@ -1,1 +1,19 @@
 console.log("Raushan")
+
+const accountId = 123432
+let accountEmail = "raushan34343@gmail.com"
+var accountPassword = "23456789"
+accountAddress = "Bhagalpur"
+
+// accountId = 333
+let accountPerson 
+accountEmail= "ravi3533@gmail.com"
+accountPassword = "339398";
+accountAddress = "Bihpur"
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountAddress, accountPerson])
+
+/*
+prefer not to use var
+because it is issue in block scope and functional scope
+*/
