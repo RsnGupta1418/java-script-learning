85df96184bc9651296ffb4482b7475b7120350b6
RsnGupta1418 <163626676+RsnGupta1418@users.noreply.github.com>
2024-03-16 15:46:20 +0000

    dataTypes

---
+++ Basic_js/dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"  // treat all code in js as a newer virsion
+
+//alert(3+5) we are using node js not browser
+
+console.log(3
+    +
+    5) // code readability is high
+
+let name= "hitesh"   
+let age =59
+let isLogin = true
+
+/*   Range 
+number => 2 to the power of 53
+bigint 
+string = " "
+boolean = true/ false
+null => standalone value => it is representation of empty value 
+undefined => 
+symbol => unique
+
+object 
+*/
+
+
+console.log(typeof "age")  // string 
+console.log(typeof age)  // number
+console.log(typeof null)  // object
+console.log(typeof undefined)  // undefined
+
